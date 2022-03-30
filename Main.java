5e58c5d39c78f7e19739ecf25fe78f9086afa2a6
Szyres <102583783+Szyres@users.noreply.github.com>
2022-03-30 11:07:17 +0200

    Opgave2 med farver uden farve filen de ønsker
    Jeg har fået lavet sådan at den nu kan plotte fraktalerne rigtigt og er begyndt på at ligge farver til dem. Jeg skal snart til at få den til at læse farve filer de har givet os for at få noget virkelig syret.

--- Main.java
+++ Main.java
@@ -1,38 +1,57 @@
 package Opgaver;
+import java.awt.Color;
 import java.util.Scanner;
 public class Main {
 	public static void main (String [] args) { 
-
 		mandal();
-
 	}
 	public static void mandal() {
-		int max = 20;
+		int max = 50;
+		StdDraw.setCanvasSize(1000,1000);
 		Scanner console = new Scanner(System.in);
 		System.out.print("Enter Grid size: ");
 		double re = console.nextDouble();
 		double im = console.nextDouble();
-		Complex center = new Complex(re,im);
-		int g = 5; 
+		double g = 512.0; 
 		Scanner console1 = new Scanner(System.in);
-		int s = console.nextInt();
+		double s = console.nextDouble();
+		
+		StdDraw.setXscale(0,g);
+		StdDraw.setYscale(0,g);
+		StdDraw.show(0);
+		
+		Complex center = new Complex(re,im);
+		for (int j = 0; j <= g-1 ; j++) { 
+			double x = (center.getReal())-(s/2)+((s*(double) j)/(g-1));
 
-
-		for (int j = 0; j < g-1 ; j++) { 
-			double x = ((center.getReal())-(s/2)+(s*j)/(g-1));
-
-			for (int k = 0; k < g-1 ; k++) { 
-				double y = ((center.getIm())-(s/2)+(s*k)/(g-1));
+			for (int k = 0; k <= g-1 ; k++) { 
+				double y = ((center.getIm())-(s/2)+(s* (double) k)/(g-1));
 
 				Complex z0 = new Complex(x,y);
-				System.out.println(z0);
-			
-
+				
+				iterate(z0);
+				if (iterate(z0) == max) { 
+					StdDraw.setPenColor(StdDraw.RED);
+					StdDraw.setPenRadius(0.5/100);
+					StdDraw.point(j+0.5,k+0.5);
+				} 
+				if (iterate(z0) < max) { 
+					StdDraw.setPenColor(StdDraw.CYAN);
+					StdDraw.setPenRadius(0.5/100);
+					StdDraw.point(j+0.5,k+0.5);
+				}	
+				if (iterate(z0) != max) { 
+					StdDraw.setPenColor(StdDraw.BLACK);
+					StdDraw.setPenRadius(0.5/100);
+					StdDraw.point(j+0.5,k+0.5);
+				}	
+				
 			}
-		}
+		} StdDraw.show(0);
+ 
 	}
 	public static int iterate(Complex z0) {
-		int max = 20; 
+		int max = 50; 
 		Complex z = new Complex(z0);
 		for (int i = 0; i < max; i++) {
 			if (z.abs() > 2.0) {
